d16e7c93507cf4ada80695355d2560352c86d01e
hryva-ihor <griva.igor422@gmail.com>
2022-04-20 12:04:39 +0300

    first_commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header_title">header</h1>
+      <p className="header_paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+        exercitationem?
+      </p>
+    </header>
+  );
+};
---
+++ src/component/Main_container.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export const Main_container = () => {
+  return (
+    <main className="main-container">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        rerum at assumenda! Esse, ea expedita quibusdam accusamus odit nam quasi
+        maiores rem delectus beatae nulla praesentium, fuga officia nobis
+        placeat!
+      </div>
+    </main>
+  );
+};
